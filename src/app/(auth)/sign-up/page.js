36798a03d8a2d0de6afd01415448689b86bb0517
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-06-12 22:18:53 +0000

    feat: sign in and sign up

--- src/app/(auth)/sign-up/page.js
+++ src/app/(auth)/sign-up/page.js
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 
+import { signUpWithCredentials } from "@/actions/auth.actions";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,37 +12,105 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { normalizeSupabaseError } from "@/utils/errors";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Enter a valid email",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
 
 export default function SignUpPage() {
+  const router = useRouter()
+  const [isSubmittingForm, setIsSubmittingForm] = useState(false);
+  const [error, setError] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = async (data) => {
+    setIsSubmittingForm(true);
+    setError("");
+    try {
+      await signUpWithCredentials(data);
+      toast.success("Account Created!")
+      router.push("/sign-in")
+    } catch (error) {
+      const friendlyMessage = normalizeSupabaseError(error.code);
+      setError(friendlyMessage);
+    } finally {
+      setIsSubmittingForm(false);
+    }
+  };
+
   return (
     <div className="py-8">
       <Card className="mx-auto max-w-sm">
         <CardHeader>
           <CardTitle className="text-2xl">Sign Up</CardTitle>
           <CardDescription>
-            Enter your email below to create an account
+            Enter your credentials to create your account
           </CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
+            {error && (
+              <div className="mb-4 text-sm bg-red-200 px-4 py-2 rounded-md">
+                <p className="text-center">{error}</p>
               </div>
-              <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
+            )}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  disabled={isSubmittingForm}
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <p className="mt-2 text-sm text-red-600">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+              <br />
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  disabled={isSubmittingForm}
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="mt-2 text-sm text-red-600">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
+              <br />
+              <Button type="submit" className="w-full">
+                {isSubmittingForm ? "Loading" : "Sign Up"}
+              </Button>
+            </form>
           </div>
           <div className="mt-4 text-center text-sm">
             Already have an account?{" "}
