3bfa76f9d60cf2a7f1fe82b0c94ac57d7ba8e31f
kirkkelvinb <kirkkelvinn@gmail.com>
2024-07-12 02:06:01 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
+
+
+
