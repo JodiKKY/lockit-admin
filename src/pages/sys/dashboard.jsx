c73b09fb87704db4ed9b62316e35d563635749b8
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-07-29 20:23:32 +0000

    updates

--- src/pages/sys/dashboard.jsx
+++ src/pages/sys/dashboard.jsx
@@ -1,16 +1,16 @@
-import {
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "@/components/ui/chart";
 import { useAuth } from "@/context/auth-context";
 import { useRooms } from "@/context/rooms-context";
 import { RoomRef } from "@/lib/firebase";
-import { addDoc, getDoc } from "firebase/firestore";
-import { ArrowDown, ArrowUp, BedIcon, HomeIcon, MailIcon, TextCursor, TriangleAlert, User2 } from "lucide-react";
+import { addDoc } from "firebase/firestore";
+import {
+  BedIcon,
+  Loader2,
+  MailIcon,
+  TextCursor,
+  TriangleAlert,
+  User2
+} from "lucide-react";
 import { useEffect } from "react";
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
-import { Loader2 } from "lucide-react";
 
 export const description = "An interactive bar chart";
 
@@ -37,40 +37,42 @@ const chartConfig = {
   },
 };
 
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-
 const Rooms = [];
 for (let i = 1; i <= 20; i++) {
   Rooms.push({
-    RoomId: `${Math.ceil(Math.random()*10000000)}`,
+    RoomId: `${Math.ceil(Math.random() * 10000000)}`,
     RoomNum: `Room ${i}`,
     isAssigned: false,
-    isAssignedTo: ""
+    isAssignedTo: "",
   });
 }
 
-console.log(Rooms)
+console.log(Rooms);
 
 export default function Dashboard() {
-  const { currentUserDetails, fetchUserDetails,loading:isFetching,setLoading:setIsFetching} = useAuth();
+  const {
+    currentUserDetails,
+    fetchUserDetails,
+    loading: isFetching,
+    setLoading: setIsFetching,
+  } = useAuth();
   const populateRooms = async () => {
     try {
       const promises = Rooms.map((room) => addDoc(RoomRef, room));
       await Promise.all(promises);
-      console.log('Rooms populated successfully.');
+      console.log("Rooms populated successfully.");
     } catch (error) {
-      console.error('Error populating rooms:', error);
+      console.error("Error populating rooms:", error);
     }
-  }
+  };
 
   useEffect(() => {
-  fetchUserDetails(currentUserDetails?.uid)
+    fetchUserDetails(currentUserDetails?.uid);
   }, []);
-
 
   console.log(currentUserDetails);
 
@@ -86,7 +88,9 @@ export default function Dashboard() {
     {
       id: 2,
       name: "Vacancies",
-      stat: `${currentUserDetails?.hotelCapacity - currentUserDetails?.bookedRooms}`,
+      stat: `${
+        currentUserDetails?.hotelCapacity - currentUserDetails?.bookedRooms
+      }`,
       icon: MailIcon,
       change: "5.4%",
       changeType: "increase",
@@ -101,8 +105,7 @@ export default function Dashboard() {
     },
   ];
 
-  const { rooms,loading } = useRooms();
-
+  const { rooms, loading } = useRooms();
 
   return (
     <div>
@@ -110,87 +113,89 @@ export default function Dashboard() {
         <h3 className="text-base font-semibold leading-6 text-gray-900">
           Dashboard
         </h3>
- {
-  loading || isFetching ?(<div className=" w-full h-[50vh] flex justify-center items-center">
-      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-  </div>):(<>
-    <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-    {stats.map((item) => (
-      <div
-        key={item.id}
-        className="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow sm:px-6 sm:pt-6"
-      >
-        <dt>
-          <div className="absolute rounded-md bg-primary p-3">
-            <item.icon
-              className="h-6 w-6 text-white"
-              aria-hidden="true"
-            />
+        {loading || isFetching ? (
+          <div className=" w-full h-[50vh] flex justify-center items-center">
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
           </div>
-          <p className="ml-16 truncate text-sm font-medium text-gray-500">
-            {item.name}
-          </p>
-        </dt>
-        <dd className="ml-16 flex items-baseline pb-6 sm:pb-7">
-          <p className="text-2xl font-semibold text-gray-900">
-            {item.stat}
-          </p>
-          <div className="absolute inset-x-0 bottom-0 bg-gray-50 px-4 py-4 sm:px-6">
-            <div className="text-sm">
-              <a
-                href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                View all
-                <span className="sr-only"> {item.name} stats</span>
-              </a>
+        ) : (
+          <>
+            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+              {stats.map((item) => (
+                <div
+                  key={item.id}
+                  className="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow sm:px-6 sm:pt-6"
+                >
+                  <dt>
+                    <div className="absolute rounded-md bg-primary p-3">
+                      <item.icon
+                        className="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <p className="ml-16 truncate text-sm font-medium text-gray-500">
+                      {item.name}
+                    </p>
+                  </dt>
+                  <dd className="ml-16 flex items-baseline pb-6 sm:pb-7">
+                    <p className="text-2xl font-semibold text-gray-900">
+                      {item.stat}
+                    </p>
+                    <div className="absolute inset-x-0 bottom-0 bg-gray-50 px-4 py-4 sm:px-6">
+                      <div className="text-sm">
+                        <a
+                          href="/rooms"
+                          className="font-medium text-indigo-600 hover:text-indigo-500"
+                        >
+                          View all
+                          <span className="sr-only"> {item.name} stats</span>
+                        </a>
+                      </div>
+                    </div>
+                  </dd>
+                </div>
+              ))}
+            </dl>
+            <div className=" mt-10 ">
+              <h3 className="text-base font-semibold leading-6 text-gray-900 mb-4">
+                Rooms
+              </h3>
+              <div className="w-full grid grid-cols-3 lg:grid-cols-10 gap-2">
+                {rooms?.map((item) => {
+                  if (item?.isAssigned === false) {
+                    return (
+                      <div
+                        key={item.RoomId}
+                        className="w-24 h-24 rounded-xl border-[1px]  flex flex-col justify-center items-center"
+                      >
+                        <BedIcon color="#2563eb" />
+                        <p className=" text-primary text-xs"> {item.RoomNum}</p>
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        key={item.RoomId}
+                        className="w-24 h-24 rounded-xl border-[1px]  flex flex-col justify-center items-center"
+                      >
+                        <TriangleAlert color="#854d0e" />
+                        <p className=" text-yellow-800 font-semibold text-xs">
+                          {item.RoomNum}
+                        </p>
+                        <span className="inline-flex items-center rounded-md bg-yellow-100 px-2 py-1 text-xs font-medium text-yellow-800">
+                          Occupied
+                        </span>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
             </div>
-          </div>
-        </dd>
-      </div>
-    ))}
-  
-
-  </dl>
-  <div className=" mt-10 ">
-  <h3 className="text-base font-semibold leading-6 text-gray-900 mb-4">
-Rooms
-  </h3>
-  <div className="w-full grid grid-cols-3 lg:grid-cols-10 gap-2">
-
-      {rooms?.map((item) => {
-        if (item?.isAssigned === false) {
-          return (
-            <div key={item.RoomId} className="w-24 h-24 rounded-xl border-[1px]  flex flex-col justify-center items-center">
-            <BedIcon color="#2563eb"/>
-     <p className=" text-primary text-xs">       {item.RoomNum}</p>
-            </div>
-          );
-          
-        }
-     else {
-      return (
-        (
-          <div key={item.RoomId} className="w-24 h-24 rounded-xl border-[1px]  flex flex-col justify-center items-center">
-          <TriangleAlert color="#854d0e"/>
-   <p className=" text-yellow-800 font-semibold text-xs">{item.RoomNum}</p>
-   <span className="inline-flex items-center rounded-md bg-yellow-100 px-2 py-1 text-xs font-medium text-yellow-800">
-  Occupied
-</span>
-          </div>
-        )
-      )
-     }
-      })}
-    </div>
-    </div>
-    </>)
- }
+          </>
+        )}
       </div>
     </div>
   );
 }
-
 
 /*       <div className="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow sm:px-6 sm:pt-6 lg:col-span-3">
             <h3 className="text-base font-semibold leading-6 text-gray-900">
